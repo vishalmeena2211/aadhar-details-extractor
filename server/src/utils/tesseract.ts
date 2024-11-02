5dd0cec875767617be661a6c21b3f953238fed89
vishalmeena2211 <meenavishal2211@gmail.com>
2024-11-02 17:27:51 +0530

    trying to fix the name field inaccuracy in ocr using regex

--- server/src/utils/tesseract.ts
+++ server/src/utils/tesseract.ts
@@ -1,11 +1,11 @@
-import Tesseract from "tesseract.js"
+import Tesseract from "tesseract.js";
 
-export const extractTextFromImage = async (buffer: Tesseract.ImageLike)=>{
+export const extractTextFromImage = async (buffer: Tesseract.ImageLike) => {
     try {
-        const {data: {text}} = await Tesseract.recognize(
+        const { data: { text } } = await Tesseract.recognize(
             buffer,
             'eng',
-        )
+        );
         return text;
     } catch (error) {
         console.error('Error during OCR:', error);
